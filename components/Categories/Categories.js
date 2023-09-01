9537bbeba0369abaf06e90ce974b450f16d5f6b6
RenanTCastro <renancastrotut@gmail.com>
2023-09-01 11:13:39 -0300

    organizando pastas

---
+++ components/Categories/Categories.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const Categories = () => {
+  return (
+    <View>
+      <Text>Hello World!</Text>
+    </View>
+  );
+};
+
+export default Categories;
